048a03c99675e804c4326a99652508836d25cd62
aowuor <angelaowuor@gmail.com>
2022-06-19 23:33:01 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
-*/
+
+ 
+// Add your pseudocode here
+  return word.split('').reverse().join('') === word ? true : false
+
 
 /*
   Add written explanation of your solution here
@@ -21,5 +22,5 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
+}
 module.exports = isPalindrome;
